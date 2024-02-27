267e504db2c48d3380f9a26bbdf01cc8dff3e751
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2024-02-27 21:59:56 +0900

    consumer, producer

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,3 @@ COPY requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . /app
-
-ENTRYPOINT [ "python", "main.py" ]
--- consumer.py
+++ consumer.py
@@ -1,5 +1,7 @@
 import pika
+import json
 from constants import RABBITMQ_URL
+from main import Shop, Order, db
 
 params = pika.URLParameters(RABBITMQ_URL)
 
@@ -10,12 +12,65 @@ channel = connection.channel()
 channel.queue_declare(queue='owner')
 
 def callback(ch, method, properties, body):
+    print('------------ Received in owner ------------')
 
-    print('Received in owner')
+    data = json.loads(body)
 
-    print(body)
+    print(data)
 
-channel.basic_consume(queue='owner', on_message_callback=callback)
+    # Shop
+
+    if properties.content_type == 'shop_created':
+        
+        shop = Shop(id=data['id'], shop_name=data['shop_name'], shop_address=data['shop_address'])
+        
+        db.session.add(shop)
+        db.session.commit()
+    
+    if properties.content_type == 'shop_updated':
+
+        shop = Shop.query.get(data['id'])
+        
+        shop.shop_name = data['shop_name']
+        shop.shop_address = data['shop_address']
+
+        db.session.commit()
+
+    if properties.content_type == 'shop_deleted':
+
+        shop = Shop.query.get(data)
+
+        db.session.delete(shop)
+        db.session.commit()
+
+    # Order
+
+    if properties.content_type == 'order_created':
+        
+        order = Order(id=data['id'], shop=data['shop'], address=data['address'])
+        
+        db.session.add(order)
+        db.session.commit()
+    
+    if properties.content_type == 'order_updated':
+
+        order = Order.query.get(data['id'])
+        
+        order.shop = data['shop']
+        order.address = data['address']
+
+        db.session.commit()
+
+    if properties.content_type == 'order_deleted':
+
+        order = Order.query.get(data)
+
+        db.session.delete(order)
+        db.session.commit()
+
+    
+
+channel.basic_consume(queue='owner', on_message_callback=callback, auto_ack=True)
 
 print('Started consuming')
 
